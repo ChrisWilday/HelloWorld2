a0601b515b1d73869110afbb290b80b1ecd96916
Crisx <wilday.chris@gmail.com>
2021-02-03 09:50:37 -0500

    goodbye

--- hello.java
+++ hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World");
+		System.out.println("Goodbye for now.");
 		
 	}
 
